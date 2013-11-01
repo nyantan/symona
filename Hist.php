c57723834aaad9cf2ea295e5a469f1f55b81c271
Yi Zhang <sakane@live.cn>
2013-11-01 17:06:56 +0800

    connection functions and hist.php

--- Hist.php
+++ Hist.php
@@ -4,20 +4,54 @@ include('Uconn.php');
 try {
 	$newlink = ulink($server, $user, $pass, $database);
 } catch (Exception $ex) {
-	echo '错误信息：' .$ex->getMessage();
+	echo $ex->getMessage();
 }
 
+// Load from loadsettings()
+	$loaded = loadsettings($newlink);
+	while ($loader = $loaded->fetch_field()) {
+		$histyear = $loader->lhconf_year;
+		$histy = array(1 => $loader->lhconf_y10,
+					   2 => $loader->lhconf_y11,
+					   3 => $loader->lhconf_y12);
+	}
+	$loaded = null;
+// END of loadsettings()
+// Load from loaddepts()
+	$loaded = loaddepts($newlink);
+	$count = 1;
+	$depts = array();
+	while ($loader = $loaded->fetch_field()) {
+		$depts[$count] = $loader->dept_name;
+	}
+// END of loaddepts()
+
+/*
+ * $histy consists of three INT(3) records, to indicate the first class and number of classes of each grade
+ * e.g. 1 => 174 means Grade 10 starts from class 17, and has 4 classes, i.e. 17, 18, 19, 20
+ */
+echo '<tr align="center">'
 $searchdate = date("Y-m-d");	// MySQL type: DATE
-
-
-while ()
-$sqlcurreval = "SELECT FROM WHERE ";	// Current evaluation period SQL
-$result = mysqli_query($newlink, $sqlcurreval);
-while($record = mysqli_fetch_array($result))
-  {
-  echo $row['FirstName'] . " " . $row['LastName'];
-  echo "<br>";
-  } 
+foreach($histy as &$yearinfo) {
+	$loopingyear = $histyear + $i;
+	$numofclasses = $yearinfo % 10;
+	for($q = 0; q < $numofclasses; q++) {
+		echo '<td>'. (string) '</td>'
+		$thisclass = ((int) $yearinfo / $numofclasses) + $q;
+		$thistotal = 0;
+		foreach($depts as &$dept) {
+			$sqlthiseval = "SELECT SUM(eval_score) FROM eval
+							WHERE eval_effective = '1' AND
+							eval_class = ". (string) $loopingyear. (string) $thisclass. "AND
+							eval_dept_name = '". (string) $dept. "'";	// Current evaluation period SQL
+			$sqlthisreturn = $newlink->query($sqlthiseval);
+			$thistotal += $sqlthisreturn->fetch_field()->SUM(eval_score);
+			echo '<td>'. (string) $sqlthisreturn->fetch_field()->SUM(eval_score). '</td>';
+		}
+		echo '<td>'. (string) $thistotal. '</td>';
+		echo '</tr>\n'
+	}
+}
 
 $sqlpreveval = 'SELECT FROM WHERE';	// Previous evaluation period SQL
 
--- Uconn.php
+++ Uconn.php
@@ -5,22 +5,51 @@
 	$pass = "nShyN4QEvuN5csat";
 	$database = "lh";
 // Setting
-	$setting = "setting";
+	$setting = "lhconf";
 
 function ulink($s, $u, $p, $d) {
 	/*
 	 * Open an Unprivileged connection to MySQL server
 	 * Parameters stands for server, username, password and database respectively
 	 */
-	$uconn = mysqli_connect($s, $u, $p, $d);
+	$uconn = new mysqli($s, $u, $p, $d);
  
-	if (mysqli_connect_errno($con)) {
-		echo '<script type="text/javascript"> alert("无法连接到数据库");</script>';
-		throw new Exception ("Cannot connect to MySQL: ". mysqli_connect_error());
+	if ($uconn -> connect_errno) {
+		echo '<script type="text/javascript"> alert("Cannot connect to database: '. $uconn->connect_error.'");</script>';
+		throw new Exception ("Cannot connect to MySQL: ". $uconn->connect_error);
 	} else {
 		return $uconn;
 	}
 }
 
-function 
+function loadsettings($settingslink) {
+	/*
+	 * TBD: QUERY CONDITION
+	 * POSSIBLE SOLUTION: BUILD AN INUSE RECORD
+	 */
+	$settingsquery = "SELECT lhconf_year, lhconf_y10, lhconf_y11, lhconf_y12 FROM lhconf 
+					  WHERE lhconf_profile == 'INUSE'";
+	// Test query and connection
+	$settingsresult = $settingslink->query($settingquery);
+	if (!settingsresult) {
+		// FAULTY CONNECTION!!! EXCEPTION!!!
+		echo '<script type="text/javascript"> alert("Cannot load server settings: '. $settingsresult->error. '")';
+		throw new Exception ("Cannot load server settings: ". $settingslink->error);
+	} else {
+		return $settingsresult;
+	}
+}
+
+function loaddepts($deptslink) {
+	$deptsquery = "SELECT dept_id FROM dept
+				   WHERE dept_id > 0";
+	$deptsquery = $deptslink->query($deptsquery);
+	if (!deptssresult) {
+		// FAULTY CONNECTION!!! EXCEPTION!!!
+		echo '<script type="text/javascript"> alert("Cannot load departments: '. $settingsresult->error. '")';
+		throw new Exception ("Cannot load departments: ". $settingslink->error);
+	} else {
+		return $deptsresult;
+	}
+}
 ?>
